000885a54d1ab874dc5ae385e75c735cb88142c1
Oh seungmin <tmdals179@gmail.com>
2024-04-13 02:25:51 +0900

    Title: 왕실의 기사 대결, Time: 165ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240413/왕실의 기사 대결/royal-knight-duel.py
+++ 240413/왕실의 기사 대결/royal-knight-duel.py
@@ -43,7 +43,6 @@ for i in range(1, N + 1):
     damage[i] = 0
 
 query = [list(map(int, input().split())) for _ in range(Q)]
-move_possible = {i: 0 for i in range(1, N + 1)}
 is_wall = {i: 0 for i in range(1, N + 1)}
 
 
@@ -54,6 +53,8 @@ def get_alive_knight():
 def get_pushed_knight(idx, d):
     dx, dy = dir_[d]
     knights = []
+    visited = [0 for _ in range(N + 1)]
+    visited[idx] = 1
     q = deque([idx])
     while q:
         i = q.popleft()
@@ -63,12 +64,16 @@ def get_pushed_knight(idx, d):
                 # 기사의 이웃한 칸이
                 if nx in [r_[i] - 1, r_[i] + h_[i]] \
                         or ny in [c_[i] - 1, c_[i] + w_[i]]:
+                    if nx in [r_[i] - 1, r_[i] + h_[i]] \
+                            and ny in [c_[i] - 1, c_[i] + w_[i]]:
+                        continue
                     # 살아 있는 기사 k의 범위에 포함된다면
                     for k in get_alive_knight():
                         if r_[k] <= nx < r_[k] + h_[k] \
                                 and c_[k] <= ny < c_[k] + w_[k]\
-                                and k not in q:
+                                and not visited[k]:
                             # knight와 q에 기사 추가
+                            visited[k] = 1
                             knights.append(k)
                             q.append(k)
     return knights
@@ -127,5 +132,26 @@ for i, d in query:
             is_wall[i] = 0
 
 # print(damage)
+# print(health)
 ans = [damage[i] for i in range(1, N + 1) if health[i] > 0]
 print(sum(ans))
+
+'''
+4 4 8
+0 0 0 0
+0 0 0 0
+0 2 0 0
+0 1 0 0
+3 1 2 1 10
+1 2 2 1 10
+1 3 1 1 10
+4 2 1 2 10
+1 0
+1 0
+1 1
+1 1
+2 2
+2 1
+2 2
+2 3
+'''
